cb2b0732884f697bdfdc75d230a848f498204ddb
only <hwy113329@163.com>
2021-11-27 15:31:35 +0800

    文档扫描和tesseract的使用

--- orc_template_match/myutils.py
+++ orc_template_match/myutils.py
@@ -1,6 +1,7 @@
 import cv2
 
-def sort_contours(cnts, method="left-to-right"):
+#
+def     sort_contours(cnts, method="left-to-right"):
     reverse = False
     i = 0
 
@@ -9,6 +10,7 @@ def sort_contours(cnts, method="left-to-right"):
 
     if method == "top-to-bottom" or method == "bottom-to-top":
         i = 1
+    #boundingBoxes是一个元组
     boundingBoxes = [cv2.boundingRect(c) for c in cnts] #用一个最小的矩形，把找到的形状包起来x,y,h,w
     (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
                                         key=lambda b: b[1][i], reverse=reverse))
